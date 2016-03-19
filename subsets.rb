c0735a8f9cf7906fd106251202216a506eb8cc07
Sameeran Kunche <skunche@gmail.com>
2016-03-19 14:58:54 -0700

    Update subsets.rb

--- subsets.rb
+++ subsets.rb
@@ -3,15 +3,6 @@ require 'byebug'
 def subsets(ary)
   return [[]] if ary.length == 0
 
-  # provides subsets by compiling lists after rejecting one element at
-  # each index
-  # subs =
-  # (0..ary.length - 1).to_a.map do |i|
-  #   ary.reject.with_index do |_, j|
-  #     j == i
-  #   end
-  # end
-
   exclude = ary.last
   subs = ary[0..-2]
 
@@ -20,10 +11,7 @@ def subsets(ary)
     subs_extra << [el] + [exclude]
   end
   subs = subs + subsets(subs)
-
-# debugger
-  # [ary] + subs.map { |sub| subsets(sub) }
-
+  
 end
 
 
